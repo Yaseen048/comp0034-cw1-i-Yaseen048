17c93d5001d3a8fa80c8818f35a0c41f3d2ff937
Yaseen <yas484@hotmail.com>
2022-02-06 17:10:23 +0000

    added style sheet and made bar graph from data

--- cinema_dash_app.py
+++ cinema_dash_app.py
@@ -5,3 +5,11 @@ import plotly.express as px
 from dash import dcc
 from dash import html
 
+external_stylesheets = [dbc.themes.BOOTSTRAP]
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+df = pd.read_csv("Prepared_dataset.csv", dtype = {'Weekend Gross': int,
+"Weeks on release": int})
+
+fig = px.bar(df, x = "Film", y = "Weekend Gross", color = "Date")
