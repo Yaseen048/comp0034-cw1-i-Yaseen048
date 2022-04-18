052231e0194ee32136e75db42f4fbd14c7f4d926
Yaseen048 <92057161+Yaseen048@users.noreply.github.com>
2022-04-18 07:19:05 +0100

    Update config.py
    added config variables

--- flask_app/config.py
+++ flask_app/config.py
@@ -1,8 +1,10 @@
-"""Flask config class."""
+import pathlib
 
 
 class Config(object):
+    """Flask config class."""
     SECRET_KEY = "xoY23504XqLK35MzQMd1Bw"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 class ProductionConfig(Config):
@@ -10,8 +12,9 @@ class ProductionConfig(Config):
 
 
 class DevelopmentConfig(Config):
-    pass
+    database_file = pathlib.Path(__file__).parent.parent.joinpath("data", 'example.sqlite')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + str(database_file)
 
 
 class TestingConfig(Config):
-    TESING = True
+    pass
