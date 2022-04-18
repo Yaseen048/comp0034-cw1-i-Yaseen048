c90b56ad91c9ddda27f8887fd4ece1b81b1a40ac
Yaseen048 <92057161+Yaseen048@users.noreply.github.com>
2022-04-18 07:24:41 +0100

    Update __init__.py
    updated create_app to pick which type of configuration

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask
 
 
-def create_app():
+def create_app(config_class_name):
     """
     Initialise the Flask application.
+    :type config_classname: Specifies the configuration class
     :rtype: Returns a configured Flask object
     """
     app = Flask(__name__)
+    app.config.from_object(config_class_name)
 
     return app
