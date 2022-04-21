ac5f9c6d6a52650405d30cc64c5c84ba1a57fc18
Yaseen048 <92057161+Yaseen048@users.noreply.github.com>
2022-04-21 12:08:27 +0100

    Update app.py
    added index function to bring to home page which include navbar

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,12 +1,14 @@
 from flask_app import create_app
 from flask_app.config import DevelopmentConfig
+from flask import render_template
 
 app = create_app(DevelopmentConfig)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+
+def index():
+    return render_template('index.html', title="Home")
 
 
 if __name__ == '__main__':
