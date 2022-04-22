eb4a9d6d33c5521e09a44d8d0c9b977f7ec63eb8
Yaseen <yas484@hotmail.com>
2022-04-22 23:19:48 +0100

    fixed singup route in auth blueprint

--- flask_app/auth/routes.py
+++ flask_app/auth/routes.py
@@ -1,7 +1,18 @@
 from flask import Blueprint
+from flask_app.auth.forms import SignupForm
+from flask import render_template
 
-auth_bp = Blueprint('auth', __name__, url_prefix='auth')
 
-auth_bp.route('/')
+auth_bp = Blueprint('auth', __name__)
+
+@auth_bp.route('/auth')
 def index():
     return "This is the authentication section of the web app"
+
+@auth_bp.route('/signup', methods=['GET', 'POST'])
+def signup():
+    form = SignupForm()
+    if form.validate_on_submit():
+        name = form.first_name.data
+        return f"Hello, {name}. You are signed up."
+    return render_template('signup.html', title='Sign Up', form=form)
