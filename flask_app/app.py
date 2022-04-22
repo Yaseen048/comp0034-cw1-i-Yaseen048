c751604847ed9efe2385d95b1160d058a9ff9ee0
Yaseen <yas484@hotmail.com>
2022-04-22 09:37:52 +0100

    made and registerd main blueprint

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,14 +1,8 @@
 from flask_app import create_app
 from flask_app.config import DevelopmentConfig
-from flask import render_template
+
 
 app = create_app(DevelopmentConfig)
-
-
-@app.route('/')
-
-def index():
-    return render_template('index.html', title="Home")
 
 
 if __name__ == '__main__':
---
+++ flask_app/main/routes.py
@@ -0,0 +1,8 @@
+from turtle import title
+from flask import Blueprint, render_template
+
+main_bp = Blueprint('main', __name__)
+
+@main_bp.route('/')
+def index():
+    return render_template('index.html', title = "Home")
