3542f481496a2894f0732906ea8a490be762c6df
Yaseen <yas484@hotmail.com>
2022-04-23 06:14:38 +0100

    made route for login

--- flask_app/auth/routes.py
+++ flask_app/auth/routes.py
@@ -1,5 +1,7 @@
+from crypt import methods
+from turtle import title
 from flask import Blueprint
-from flask_app.auth.forms import SignupForm
+from flask_app.auth.forms import LoginForm, SignupForm
 from flask import render_template
 
 
@@ -16,3 +18,9 @@ def signup():
         name = form.first_name.data
         return f"Hello, {name}. You are signed up."
     return render_template('signup.html', title='Sign Up', form=form)
+
+@auth_bp.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm
+    #validation code
+    return render_template('login.html', title='Login', form=form)
