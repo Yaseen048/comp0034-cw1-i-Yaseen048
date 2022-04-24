546f33a6f8d769a0bf322f6b342b1c89b729642a
Yaseen <yas484@hotmail.com>
2022-04-24 06:16:39 +0100

    created sqlalchemy object and intialised it

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -1,8 +1,9 @@
 from flask import Flask
 from flask_wtf.csrf import CSRFProtect
+from flask_sqlalchemy import SQLAlchemy
 
 csrf = CSRFProtect()
-
+db = SQLAlchemy()
 
 def create_app(config_class_name):
     """
@@ -13,6 +14,7 @@ def create_app(config_class_name):
     app = Flask(__name__)
     app.config.from_object(config_class_name)
     csrf.init_app(app)
+    db.init_app(app)
 
     from flask_app.main.routes import main_bp
     app.register_blueprint(main_bp)
