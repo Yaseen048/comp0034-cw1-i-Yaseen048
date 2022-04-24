196f64a4e7bd8a5e209227f112e654398c9aed4d
Yaseen <yas484@hotmail.com>
2022-04-24 23:45:41 +0100

    created object for login manager and intialised it

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask_wtf.csrf import CSRFProtect
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 csrf = CSRFProtect()
 db = SQLAlchemy()
+Login_manager = LoginManager()
 
 def create_app(config_class_name):
     """
@@ -15,6 +17,8 @@ def create_app(config_class_name):
     app.config.from_object(config_class_name)
     csrf.init_app(app)
     db.init_app(app)
+    Login_manager.init_app(app)
+    Login_manager.login_view = 'auth.login'
 
     with app.app_context():
         from flask_app.models import User
