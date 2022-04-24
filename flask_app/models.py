3f0412ac48081260325c33f35e34b8300a3f0c8a
Yaseen <yas484@hotmail.com>
2022-04-24 23:48:40 +0100

    inherit usermixin for User class

--- flask_app/models.py
+++ flask_app/models.py
@@ -1,8 +1,8 @@
 from flask_app import db
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
-
-class User(db.Model):
+class User(UserMixin, db.Model):
     __tablename__ = "user"
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.Text, nullable=False)
