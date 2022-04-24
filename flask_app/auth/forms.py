70b3113429a76e3eee03b81b58d0d7c8fdb6930e
Yaseen <yas484@hotmail.com>
2022-04-24 08:10:03 +0100

    added logina nd sigunp email validation

--- flask_app/auth/forms.py
+++ flask_app/auth/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, EmailField, SubmitField
-from wtforms.validators import DataRequired, EqualTo
+from wtforms.validators import DataRequired, EqualTo, ValidationError
+from flask_app.models import User
 
 class SignupForm(FlaskForm):
     first_name = StringField(label='First name', validators=[DataRequired()])
@@ -10,9 +11,18 @@ class SignupForm(FlaskForm):
     password_repeat = PasswordField(label='Repeat Password',
                                 validators=[DataRequired(), EqualTo('password', message='Passwords must match')])
                         
-
+    def validate_email(self, email):
+        users = User.query.filter_by(email=email.data).first()
+        if users is not None:
+            raise ValidationError('An account is already registered for that email address')
 
 class LoginForm(FlaskForm):
     email = EmailField(label='Email address', validators=[DataRequired()])
     password = PasswordField(label='Password', validators=[DataRequired()])
     submit = SubmitField('Login in')
+
+    def validate_login(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is None:
+            raise ValidationError('This email is not registered')
+        
