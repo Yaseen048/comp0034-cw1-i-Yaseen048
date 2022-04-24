683ab4aad3c95c6fd411c6a28d49d30ab538bd66
Yaseen <yas484@hotmail.com>
2022-04-24 08:42:54 +0100

    edited signup route by adding user to database

--- flask_app/auth/routes.py
+++ flask_app/auth/routes.py
@@ -1,6 +1,10 @@
 from flask import Blueprint, redirect, url_for
 from flask_app.auth.forms import SignupForm, LoginForm
 from flask import render_template, flash
+from sqlalchemy.exc import IntegrityError
+from flask_app import db
+from flask_app.models import User
+
 
 
 auth_bp = Blueprint('auth', __name__)
@@ -13,8 +17,16 @@ def index():
 def signup():
     form = SignupForm()
     if form.validate_on_submit():
-        name = form.first_name.data
-        flash (f"Hello, {name}. You are signed up.")
+        user = User(first_name=form.first_name.data, last_name=form.last_name.data, email=form.email.data)
+        user.set_password(form.password.data)
+        try:
+            db.session.add(user)
+            db.session.commit()
+            flash (f"Hello, {user.first_name}. You are signed up.")
+        except IntegrityError:
+            db.session.rollback()
+            flash(f"There was an error. Unable to register {form.email.data}.",'Error')
+            return redirect(url_for('auth.signup'))
         return redirect(url_for('main.index'))
     return render_template('signup.html', title='Sign Up', form=form)
 
