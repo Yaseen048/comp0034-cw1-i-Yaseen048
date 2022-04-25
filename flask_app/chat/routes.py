fb42eb0f8d054b844e84e9ecc5360bbc90016481
Yaseen <yas484@hotmail.com>
2022-04-25 08:07:45 +0100

    made chat only accesible if user is logged in

--- flask_app/chat/routes.py
+++ flask_app/chat/routes.py
@@ -1,8 +1,18 @@
-from turtle import title
-from flask import Blueprint, render_template, flash, redirect, url_for
+from flask import Blueprint, render_template
+from flask_login import login_required
+from flask_app import login_manager
+from flask_app.models import User
 
 chat_bp = Blueprint('chat', __name__)
 
-@chat_bp.route('/')
+@login_manager.user_loader
+def load_user(user_id):
+    """ Takes a user ID and returns a user object or None if the user does not exist"""
+    if user_id is not None:
+        return User.query.get(user_id)
+    return None
+
+@chat_bp.route('/view')
+@login_required
 def view():
     return render_template('chat.html', title = 'chat')
