acc5a6c9a4e149b661e804fb48dd288e78656b86
Yaseen <yas484@hotmail.com>
2022-04-25 07:40:59 +0100

    made chat view page

--- flask_app/chat/routes.py
+++ flask_app/chat/routes.py
@@ -1,6 +1,8 @@
+from turtle import title
 from flask import Blueprint, render_template, flash, redirect, url_for
 
 chat_bp = Blueprint('chat', __name__)
 
 @chat_bp.route('/')
-def
+def view():
+    return render_template('chat.html', title = 'chat')
