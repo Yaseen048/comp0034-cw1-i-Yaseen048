5c8f46d98d9278146f9bdcec7202ea2ed67cf8ac
Yaseen <yas484@hotmail.com>
2022-04-25 06:00:06 +0100

    added login/register/logout button to navbar

--- flask_app/main/routes.py
+++ flask_app/main/routes.py
@@ -1,8 +1,11 @@
-from turtle import title
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, flash
+from flask_login import current_user
 
 main_bp = Blueprint('main', __name__)
 
 @main_bp.route('/')
 def index():
+    if not current_user.is_anonymous:
+        name = current_user.first_name
+        flash(f'Hello {name}, you are now logged in')
     return render_template('index.html', title = "Home")
