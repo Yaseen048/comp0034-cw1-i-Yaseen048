9e8c130696b4c923d97544d246ff4ec8cdaa4b79
Yaseen <yas484@hotmail.com>
2022-04-25 09:05:45 +0100

    made message function to add messages to db

--- flask_app/chat/routes.py
+++ flask_app/chat/routes.py
@@ -1,8 +1,13 @@
-from flask import Blueprint, render_template
-from flask_login import login_required
-from flask_app import login_manager
+from flask import Blueprint, render_template, flash, redirect, url_for
+from flask_login import login_required, current_user
+
+from flask_app import login_manager, db
 from flask_app.models import User
 from flask_app.chat.forms import MessageForm
+from flask_app.models import Message
+
+from sqlalchemy.exc import IntegrityError
+
 
 chat_bp = Blueprint('chat', __name__)
 
@@ -18,8 +23,18 @@ def load_user(user_id):
 def view():
     return render_template('chat.html', title = 'chat')
 
-@chat_bp.route('/message')
+@chat_bp.route('/message', methods = ['GET', 'POST'])
 @login_required
 def message():
     message_form = MessageForm()
-    
+    if message_form.validate_on_submit():
+        message = Message(author_id = current_user.id, message_text = message_form.message.data)
+        try:
+            db.session.add(message)
+            db.session.commit()
+        except IntegrityError:
+            db.session.rollback()
+            flash(f"There was an error. Unabke to upload message",'Error')
+            return redirect(url_for('chat.message'))
+        return redirect(url_for('chat.view'))
+    return render_template('write_message.html', title='Write message', form=message_form)
